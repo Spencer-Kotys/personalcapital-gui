a2447c7ec0e3b544c8850ef187f0484779771080
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-04-12 22:40:26 -0400

    Adding personal capital api

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
-print("$100")
+from personalcapital import PersonalCapital, RequireTwoFactorException, TwoFactorVerificationModeEnum
+import time
+import json
+import logging
+
+class PewCapital(PersonalCapital):
+    """
+    Extends PersonalCapital to save and load session
+    So that it doesn't require 2-factor auth every time
+    """
+    def __init__(self):
+        PersonalCapital.__init__(self)
+        self.__session_file = 'session.json'
+
+    def load_session(self):
+        try:
+            with open(self.__session_file) as data_file:
+                cookies = {}
+                try:
+                    cookies = json.load(data_file)
+                except ValueError as err:
+                    logging.error(err)
+                self.set_session(cookies)
+        except IOError as err:
+            logging.error(err)
+
+    def save_session(self):
+        with open(self.__session_file, 'w') as data_file:
+            data_file.write(json.dumps(self.get_session()))
+
+
+pc = PewCapital()
+pc.load_session()
+
+emailFile = open("email.txt", "r")
+passwordFile = open("password.txt", "r")
+
+email = emailFile.read()
+password = passwordFile.read()
+
+try:
+    pc.login(email, password)
+except RequireTwoFactorException:
+    pc.two_factor_challenge(TwoFactorVerificationModeEnum.SMS)
+    pc.two_factor_authenticate(TwoFactorVerificationModeEnum.SMS, input('code: '))
+    pc.authenticate_password(password)
+
+accounts_response = pc.fetch('/newaccount/getAccounts')
+accounts = accounts_response.json()['spData']
+
+print('Networth:{0}'.format(accounts['networth']))
+
+pc.save_session()
--- script.js
+++ script.js
@@ -4,14 +4,14 @@ var pyshell = require('python-shell')
 function api() {
   console.log("script.js")
   document.getElementById("net").innerHTML = "Net Worth $1"
-  document.getElementById("assets").innerHTML = "Net Worth $1"
-  document.getElementById("liabilities").innerHTML = "Net Worth $1"
-  document.getElementById("investments").innerHTML = "Net Worth $1"
+  document.getElementById("assets").innerHTML = "Assets $1"
+  document.getElementById("liabilities").innerHTML = "Liabilities $1"
+  document.getElementById("investments").innerHTML = "Investments $1"
   pyshell.PythonShell.run('main.py', null, function (err, results) {
     if (err) err
     console.log('main.py done')
     results = results.toString()
     console.log('results', results)
-    document.getElementById("net").innerHTML = 'Net Worth ' + results
+    document.getElementById("net").innerHTML = results
   })
 }
