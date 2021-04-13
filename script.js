c49161516da5c705f60f07ec0295e69438f811c7
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-04-12 22:06:02 -0400

    Testing innerHTML

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var pyshell = require('python-shell')
 
 function api() {
   console.log("script.js")
+  document.getElementById("net").innerHTML = "Net Worth $1"
+  document.getElementById("assets").innerHTML = "Net Worth $1"
+  document.getElementById("liabilities").innerHTML = "Net Worth $1"
+  document.getElementById("investments").innerHTML = "Net Worth $1"
   pyshell.PythonShell.run('main.py', null, function (err, results) {
     if (err) err
     console.log('main.py done')
