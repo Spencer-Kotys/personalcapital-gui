42dcbd56a2057da06841b237afe1c1ad974cf835
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-05-05 22:47:34 -0400

    Getting input from email and password fields

--- script.js
+++ script.js
@@ -20,4 +20,8 @@ function api() {
 
 function login() {
   console.log("hello world")
+  var email = document.getElementById("email").value
+  console.log(email)
+  var password = document.getElementById("password").value
+  console.log(password)
 }
