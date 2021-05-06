3bcf88e4df99c2f10e172ee5782ffb7850e11825
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-05-05 23:03:35 -0400

    Creating login.py from main.py

---
+++ login.py
@@ -0,0 +1,55 @@
+from personalcapital import PersonalCapital, RequireTwoFactorException, TwoFactorVerificationModeEnum
+import time
+import json
+import logging
+import sys # for input from javascript
+
+class PewCapital(PersonalCapital):
+    """
+    Extends PersonalCapital to save and load session
+    So that it doesn't require 2-factor auth every time
+    """
+    def __init__(self):
+        PersonalCapital.__init__(self)
+        self.__session_file = 'session.json'
+
+    def load_session(self):
+        try:
+            with open(self.__session_file) as data_file:
+                cookies = {}
+                try:
+                    cookies = json.load(data_file)
+                except ValueError as err:
+                    logging.error(err)
+                self.set_session(cookies)
+        except IOError as err:
+            logging.error(err)
+
+    def save_session(self):
+        with open(self.__session_file, 'w') as data_file:
+            data_file.write(json.dumps(self.get_session()))
+
+
+pc = PewCapital()
+pc.load_session() # load session from json
+
+email = sys.argv[1] # Takes first argument
+password = sys.argv[2] # Takes second argument
+
+try:
+    pc.login(email, password)
+except RequireTwoFactorException:
+    pc.two_factor_challenge(TwoFactorVerificationModeEnum.SMS)
+    pc.two_factor_authenticate(TwoFactorVerificationModeEnum.SMS, input('code: '))
+    pc.authenticate_password(password)
+
+accounts_response = pc.fetch('/newaccount/getAccounts')
+
+try:
+    accounts = accounts_response.json()['spData']
+except:
+    print("Login Failed")
+else:
+    print("Login Succeeded")
+
+pc.save_session() # save session to json
