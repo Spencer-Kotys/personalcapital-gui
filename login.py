0dc250317c4ea073f2c54c56e11201db8aaaef8a
Spencer-Kotys <spencerkotys@gmail.com>
2021-06-08 15:10:38 -0800

    Changing login.py; It now creates files

--- login.py
+++ login.py
@@ -1,23 +1,20 @@
-from personalcapital import PersonalCapital, TwoFactorVerificationModeEnum
-import time
-import json
 import sys # for input from javascript
-
-pc = PersonalCapital()
 
 email = sys.argv[1] # Takes first argument
 password = sys.argv[2] # Takes second argument
 
 try:
-    pc.login(email, password)
+    emailfile = open("email.txt", "w")
+    emailfile.write(email)
 except:
-    print("TwoFactorRequired")
-    pc.two_factor_challenge(TwoFactorVerificationModeEnum.SMS)
+    print("Creating email file failed")
 else:
-    accounts_response = pc.fetch('/newaccount/getAccounts')
-    try:
-        accounts = accounts_response.json()['spData']
-    except:
-        print("Login Failed")
-    else:
-        print("Login Succeeded")
+    print("Creating email file succeded")
+
+try:
+    passwordfile = open("password.txt", "w")
+    passwordfile.write(password)
+except:
+    print("Creating password file failed")
+else:
+    print("Creating password file succeded")
